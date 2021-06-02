83965644b52f795ee5dd088e036cd9486905ee24
丁龙强 <dlqym@qq.com>
2021-06-02 13:46:54 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="index">
+    <text>密码输入的 input</text>
+    <input type="password" password placeholder="这是一个密码输入框"/>
+  </view>
+</template>
+
+<script>
+import './index.scss'
+
+export default {
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+}
+</script>
